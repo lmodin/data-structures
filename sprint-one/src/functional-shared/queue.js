71012ae1c6943bd94dc00ec880ca4f7ed44d3487
lmodin <lisamodin@live.com>
2020-05-13 16:21:36 -0700

    Updating file with queue and stack under functional shared both passing all tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.storage = {};
+  _.extend(newQueue, queueMethods);
+
+  return newQueue;
+
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  var defaultKey = Object.keys(this.storage).length
+  while (this.storage[defaultKey] !== undefined) {
+    defaultKey++;
+  }
+  this.storage[defaultKey] = value
+}
+
+queueMethods.dequeue = function() {
+  var first = Object.keys(this.storage)[0];
+  var value = this.storage[first];
+  delete this.storage[first];
+  return value;
+}
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+}
+
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.storage = {};
+  _.extend(newStack, stackMethods);
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  var defaultKey = Object.keys(this.storage).length;
+  while (this.storage[defaultKey] !== undefined) {
+    defaultKey++;
+  }
+  this.storage[defaultKey] = value;
+}
+
+stackMethods.pop = function() {
+  var last = Object.keys(this.storage)[Object.keys(this.storage).length - 1];
+  var value = this.storage[last];
+  delete this.storage[last];
+  return value;
+}
+
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+}
 
