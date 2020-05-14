071212694165c90ebcc58a257258c8c9c54e57aa
lmodin <lisamodin@live.com>
2020-05-13 21:05:02 -0700

    Adding queue and stack for prototypal pattern with all tests passing

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.storage = {};
+  return newQueue
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  var defaultKey = Object.keys(this.storage).length;
+  while(this.storage[defaultKey] !== undefined) {
+    defaultKey ++;
+  };
+  this.storage[defaultKey] = value;
+};
 
+queueMethods.dequeue = function() {
+  var firstKey = Object.keys(this.storage)[0];
+  var value = this.storage[firstKey];
+  delete this.storage[firstKey];
+  return value;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  var thisKey = Object.keys(this.storage).length;
+  while (this.storage[thisKey] !== undefined) {
+    thisKey ++;
+  }
+  this.storage[thisKey] = value;
+};
 
+stackMethods.pop = function() {
+  var last = Object.keys(this.storage)[Object.keys(this.storage).length - 1];
+  var value = this.storage[last];
+  delete this.storage[last];
+  return value;
+};
+
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+}
