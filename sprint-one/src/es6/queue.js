f6cad5c8f8393c9634bbdca023f24f4760c60b42
lmodin <lisamodin@live.com>
2020-05-13 21:46:42 -0700

    Updating queue and stack in es6 passing pomander sniff test

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -9,19 +9,19 @@ class Queue {
     var thisKey = Object.keys(this.storage).length;
     while (this.storage[thisKey] !== undefined) {
       thisKey ++;
-    };
+    }
     this.storage[thisKey] = value;
-  };
+  }
 
   dequeue() {
     var first = Object.keys(this.storage)[0];
     var value = this.storage[first];
     delete this.storage[first];
     return value;
-  };
+  }
 
   size() {
     return Object.keys(this.storage).length;
-  };
+  }
 
 }
