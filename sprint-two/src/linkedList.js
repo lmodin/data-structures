f067fcc147811b666104cc4a57da86a3b48f9151
lmodin <lisamodin@live.com>
2020-05-14 15:44:29 -0700

    Adding LinkedList with all tests passing, and time complexity question answered

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,16 +4,41 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    node = Node(value);
+
+    if (!list.head) {
+      list.head = node;
+    } else {
+      list.tail.next = node;
+    }
+    list.tail = node;
+
   };
 
   list.removeHead = function() {
+    //save the current head...to be return
+    var saveValue = list.head.value;
+    delete list.head.value;
+
+    list.head = list.head.next;
+    return saveValue;
   };
 
   list.contains = function(target) {
+    var start = list.head;
+    while(start !== null) {
+      if (start.value ===target) {
+        return true;
+      }
+      start = start.next;
+    }
+    return false;
   };
 
   return list;
 };
+
+
 
 var Node = function(value) {
   var node = {};
@@ -26,4 +51,6 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  Contains is linear
+  remove head and add tail are constant
  */
