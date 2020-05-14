4b97450f665140bb26c3e89044819fadd7aa13ed
lmodin <lisamodin@live.com>
2020-05-13 21:38:16 -0700

    Adding queue and stack for es6, with all tests passing

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
+
+  enqueue(value) {
+    var thisKey = Object.keys(this.storage).length;
+    while (this.storage[thisKey] !== undefined) {
+      thisKey ++;
+    }
+    this.storage[thisKey] = value;
+  };
+
+  dequeue() {
+    var first = Object.keys(this.storage)[0];
+    var value = this.storage[first];
+    delete this.storage[first];
+    return value;
+  };
+
+  size() {
+    return Object.keys(this.storage).length;
+  };
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    var thisKey = Object.keys(this.storage).length;
+    while (this.storage[thisKey] !== undefined) {
+      thisKey ++;
+    };
+    this.storage[thisKey] = value;
+  }
+
+  pop() {
+    var last = Object.keys(this.storage)[Object.keys(this.storage).length - 1];
+    var result = this.storage[last];
+    delete this.storage[last];
+    return result;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
